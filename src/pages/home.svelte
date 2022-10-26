ed1e271e5a26db9b8e4125ca7af267d99f8fc29e
Tom Wilson <tom@hyper.io>
2022-10-26 12:35:00 +0000

    first commit

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,3 +1,9 @@
+<script>
+  import Connect from "../dialogs/connect.svelte";
+
+  let open = false;
+</script>
+
 <nav
   class="flex space-x-4 h-[75px] bg-secondary text-secondary-content flex items-center"
 >
@@ -9,6 +15,8 @@
     <div class="hero-content flex-col">
       <h1 class="text-8xl font-bold">⚡️HypAR⚡️</h1>
       <p>The Ultimate Permaweb Stack!</p>
+      <button class="btn" on:click={() => (open = true)}>Open Modal</button>
     </div>
   </section>
 </main>
+<Connect bind:open />
